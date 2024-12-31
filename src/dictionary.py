1414fc1ab6cfe4411e5009c9195606c8a061b2b5
Simon Gao <simon.shubin@gmail.com>
2024-12-31 15:38:53 +0100

    🐛 Fixed dictionary to also track the doc occurences

--- src/dictionary.py
+++ src/dictionary.py
@@ -2,54 +2,28 @@
 
 import os
 from gensim import corpora
-# Import libraries
-from gensim import corpora
-from gensim.models import LdaModel, HdpModel, LsiModel
-from gensim.parsing.preprocessing import (
-    preprocess_string,
-    strip_punctuation,
-    strip_numeric,
-    remove_stopwords,
-    strip_short,
-    stem_text,
-)
 from tqdm import tqdm
-from nltk.stem.snowball import SnowballStemmer
+import preprocessing
 
-import nltk
-
-
-filters = [
-    strip_punctuation,  # Remove punctuation
-    strip_numeric,  # Remove numbers
-    remove_stopwords,  # Remove stopwords
-    strip_short,  # Remove words shorter than 3 characters
-]
+interactive = int(os.environ.get('INTERACTIVE', '0')) == 1
 
 
-nltk.download('stopwords')
-germanStopwords = nltk.corpus.stopwords.words('german')
-snowball = SnowballStemmer("german")
-
-dictionary = corpora.Dictionary()
 
 files = os.listdir("texts")
-files = files[:1]
 
 english = False
 
-for i in tqdm(range(len(files))):
-    with open(os.path.join("texts", files[i])) as file:
-        lines = ' '.join(file.readlines())
+def generator():
+    for i in tqdm(range(len(files))):
+        with open(os.path.join("texts", files[i])) as file:
+            lines = ' '.join(file.readlines())
+            tokens = preprocessing.stringToTokens(lines, english)
+            yield tokens
 
-        tokens = preprocess_string(lines, filters)
-        # Apply stemming to each token
-        stemmed_tokens = [stem_text(token) for token in tokens] if english else [snowball.stem(token) for token in tokens]
-        if not english:
-            print('stemming german')
-            stemmed_tokens = [w for w in stemmed_tokens if w not in germanStopwords]
-        print(stemmed_tokens)
-        dictionary.add_documents([stemmed_tokens])
+dictionary = corpora.Dictionary(generator())
 
-dictionary.save(input('save to filename: '))
+filename =  input('save to filename: ') if interactive else os.getenv('DICT_FILENAME', 'no-name.dict')
+
+dictionary.save(filename)
+dictionary.save_as_text(filename + ".dbg")
 
