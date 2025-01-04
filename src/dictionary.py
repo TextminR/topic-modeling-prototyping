f4abd4ada35395a2333b2391bdb1258787d51e7c
Simon Gao <simon.shubin@gmail.com>
2025-01-04 16:02:01 +0100

    ✨ Adapted dictionary.py to work with preprocessed

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,29 +1,36 @@
 """creates a dictionary from the folder"""
 
 import os
+from dotenv import load_dotenv
 from gensim import corpora
 from tqdm import tqdm
 import preprocessing
 
+load_dotenv()
 interactive = int(os.environ.get('INTERACTIVE', '0')) == 1
 
-
-
-files = os.listdir("texts")
+folder = input('folder: ') if interactive else os.getenv('TEXT_FOLDER', 'texts')
+files = os.listdir(folder)
 
 english = False
+preprocessed = True
 
 def generator():
+    print('creating dictionary from folder: ' + folder)
     for i in tqdm(range(len(files))):
-        with open(os.path.join("texts", files[i])) as file:
-            lines = ' '.join(file.readlines())
-            tokens = preprocessing.stringToTokens(lines, english)
+        with open(os.path.join(folder, files[i])) as file:
+            if not preprocessed:
+                lines = ' '.join(file.readlines())
+                tokens = preprocessing.stringToTokens(lines, english)
+            else:
+                tokens = file.readline()
             yield tokens
 
 dictionary = corpora.Dictionary(generator())
 
 filename =  input('save to filename: ') if interactive else os.getenv('DICT_FILENAME', 'no-name.dict')
 
+print('saving dictionary to: ' + filename)
 dictionary.save(filename)
 dictionary.save_as_text(filename + ".dbg")
 
